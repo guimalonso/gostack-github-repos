f25a1994074434bf857e31e1ebce0b8c1de9e53e
Guilherme Alonso <guilherme.malonso@gmail.com>
2019-07-25 19:36:11 -0300

    Updates

---
+++ precache-manifest.a679e754eeed37f0b9bcf5ecdec56b47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c92a41ea0416eb6e6c8f0292afe5b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb588aa9c584cd331e6d",
+    "url": "/static/js/2.9032265f.chunk.js"
+  },
+  {
+    "revision": "ca0b14a0f4ef3b5f7eec",
+    "url": "/static/js/main.b7a0cb0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
