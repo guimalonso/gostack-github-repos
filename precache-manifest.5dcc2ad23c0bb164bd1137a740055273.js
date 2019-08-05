f3ad3f1d0d81e02ffdc192affaca43ce9f97f80c
Guilherme Alonso <guilherme.malonso@gmail.com>
2019-08-05 19:26:42 -0300

    Updates

---
+++ precache-manifest.5dcc2ad23c0bb164bd1137a740055273.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1972758b6d026ecb8343da2d32bc6fd",
+    "url": "/gostack-github-repos/index.html"
+  },
+  {
+    "revision": "a032fcec9fe88259431d",
+    "url": "/gostack-github-repos/static/js/2.8dc4fc93.chunk.js"
+  },
+  {
+    "revision": "ca0b14a0f4ef3b5f7eec",
+    "url": "/gostack-github-repos/static/js/main.b7a0cb0a.chunk.js"
+  },
+  {
+    "revision": "b4d6e0ea5f3ed1a2143e",
+    "url": "/gostack-github-repos/static/js/runtime~main.11fc78f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a679e754eeed37f0b9bcf5ecdec56b47.js"
+  "/gostack-github-repos/precache-manifest.5dcc2ad23c0bb164bd1137a740055273.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gostack-github-repos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
